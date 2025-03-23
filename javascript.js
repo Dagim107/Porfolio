f583d3175a86643a5a4a4f27015b1aa807e76ec4
Dagim Demeke <dagim1077@gmail.com>
2025-03-22 20:44:42 -0500

    Spelling correction

--- javascript.js
+++ javascript.js
@@ -3,4 +3,4 @@ console.log('I am a Developer');
 
 //document.getElementById("skills").textContent = 'hello';
 
-window.alert('You are looking at my Perfolio!');
+window.alert('You are looking at my Portfolio!');
